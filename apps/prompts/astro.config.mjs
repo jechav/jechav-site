8034402fe8359052433e3ef8bb49b29410141f31
Jose Echavez M <jose.echavez92@gmail.com>
2026-06-10 17:07:54 -0500

    fix redirection issue when login

--- apps/prompts/astro.config.mjs
+++ apps/prompts/astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  output: "server",
+  adapter: node({ mode: "standalone" }),
+  allowedDomains: [{ hostname: "prompts.jechav.me", protocol: "https" }],
 });
